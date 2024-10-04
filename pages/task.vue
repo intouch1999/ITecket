f363442071163ae99ce957099b66e4f05a893f08
intouch S <147333368+intouch1999@users.noreply.github.com>
2024-10-04 17:19:58 +0700

    create task page

---
+++ pages/task.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl card bg-yellow-200 p-4 mx-auto mb-4 border-b-2 border-gray-200 shadow-md">
+        <div class="flex">
+            <h1 class="text-2xl mx-auto">แจ้งปัญหาการใช้งาน</h1>
+        </div> 
+         <div class="w-full mx-auto my-4">
+            <label class="form-control w-full max-w-xs ">
+  <div class="label">
+    <span class="label-text">What is your name?</span>
+  </div>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</label>
+<label class="form-control w-full max-w-xs ">
+  <div class="label">
+    <span class="label-text">What is your name?</span>
+  </div>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</label>
+<label class="form-control w-full max-w-xs ">
+  <div class="label">
+    <span class="label-text">What is your name?</span>
+  </div>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</label>
+</div>  
+    </div>
+</template>
