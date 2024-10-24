92259fc09a46c9072ea83277086531e82f9f6804
intouch S <147333368+intouch1999@users.noreply.github.com>
2024-10-24 17:46:49 +0700

    set layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,33 @@
     <div class="container mx-auto w-full mb-4">
     </div>
     <div class="container mx-auto w-4/5 mb-4 card bg-primary rounded-2xl">
-        <div class="w-1/2 flex flex-row p-4">
+        <div class="w-full flex flex-row p-4">
             <div role="tablist" class="tabs tabs-boxed">
-                <a role="tab" class="tab">กำลังดำเนินการ</a>
+                <a role="tab" class="tab">ดำเนินการ</a>
                 <a role="tab" class="tab tab-active">เสร็จสิ้น</a>
                 <a role="tab" class="tab">ยกเลิก</a>
             </div>
         </div>
         <div v-for="(task, index) in FormatTask " :key="index"
-            class="p-4 mx-4 flex flex-row card bg-secondary my-2 justify-between items-center">
-            <div class="grid-cols-1">
-                <div class="badge badge-info ">
+            class="relative p-4 mx-4 flex flex-row flex-wrap text-xs card bg-secondary my-2 justify-center items-center ">
+            <div class="w-1/3">
+                <input type="checkbox" class="checkbox"/>
+            </div>
+            <div class="absolute -top-2 left-5 w-2/6 inline-block">
+                <div class=" badge badge-info ">
                     {{ task.status }}
                 </div>
             </div>
-            <div class="basis-1/6  px-4 inline-block">
+            <div class="w-full mt-2">
                 {{ task.formatedTask }}.
             </div>
-            <div class="flex flrx-row">
-
-            
-            <div class="basis-2/3  px-4">
+            <div class="w-2/3 mt-2">
                 {{ task.limitText }}
             </div>
 
-            <div class="basis-1/6  px-4">
+            <div class="w-1/3 text-center">
                 action
             </div>
-        </div>
         </div>
     </div>
 
