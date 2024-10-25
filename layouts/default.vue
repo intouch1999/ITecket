672edd36fe2d18a38e10245c67a599b3247ba9e8
intouch S <147333368+intouch1999@users.noreply.github.com>
2024-10-25 17:30:57 +0700

    change nuxt link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" mx-auto mb-4 border-b-2 border-gray-200 shadow-md ">
+  <div class=" mx-auto border-b-2 border-red-300 shadow-md ">
     <div class="navbar bg-primary text-primary-content w-full ">
       <div class="navbar-start">
         <div class="dropdown">
@@ -9,14 +9,14 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </div>
-          <ul tabindex="0" class=" menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <ul tabindex="0" class="   menu menu-lg w-52 dropdown-content bg-base-100 rounded-box z-20 mt-3 p-2 shadow">
             <li v-for="nav_path in navPaths" :key="nav_path.path">
               <NuxtLink :to="nav_path.path" :class="{ 'bg-secondary ': isActive(nav_path.path) }">{{ nav_path.name }}
               </NuxtLink>
             </li>
           </ul>
         </div>
-        <a class="btn btn-ghost text-xl">daisyUI</a>
+        <NuxtLink :to="'/'" class="btn btn-ghost text-xl">daisyUI</NuxtLink>
       </div>
       <div class="navbar-center hidden lg:flex">
         <ul v-for="nav_path in navPaths" :key="nav_path.path">
