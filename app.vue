2ab5745d3785e4b62d2570bb76c675c744b2aaf4
intouch S <147333368+intouch1999@users.noreply.github.com>
2024-10-30 16:10:36 +0700

    add meta

--- app.vue
+++ app.vue
@@ -3,3 +3,27 @@
     <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'ITicket',
+  ogTitle: 'ITicket',
+  description: 'IT Ticket system for IT support',
+  ogDescription: 'IT Ticket system for IT support',
+})
+
+useHead({
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/img/photo.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/photo.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/photo.png' },
+    ],
+    meta: [
+      { name: 'theme-color', content: '#000000' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+      { name: 'apple-mobile-web-app-title', content: 'ITicket' },
+    ],
+  })
+</script>
